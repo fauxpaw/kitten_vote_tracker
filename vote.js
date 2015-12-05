5f135a8fad7e8a8f1882079d3575f38a980362a6
Michael <fauxpawsc@live.com>
2015-12-05 01:26:46 -0600

    filter picks based on previous selections and duplicate

--- vote.js
+++ vote.js
@@ -1,6 +1,6 @@
 var photo_count = 14;
 var photoArray = [];
-var catsThisSession = [];
+var picksThisSession = [];
 
 function Photo (number) {
 	this.number = number;
@@ -13,33 +13,50 @@ function initializePhotos (){
 	for (i=0; i < photo_count; i++){
 		photoArray[i]  = new Photo(i);
 		photoArray[i].src = 'images/' + i + '.jpg';
-		$('#img'+i).attr('src', photoArray[i].src);
+		// use this to check if all images are displaying properly
+		//$('#img'+i).attr('src', photoArray[i].src);
 	}
 }
 
 //generate random numbers for photos
-function randomTwoPics (){
-		var firstPick = Math.floor(Math.random()*(14));
-		var secondPick = Math.floor(Math.random()*(14));
-		console.log(secondPick);
-		console.log(firstPick);
-	if(firstPick === secondPick){
-		randomTwoPics();
+function randomTwoPicks (){
+	//to get range of 0-13 use the allgorithm below
+	var firstPick = Math.floor(Math.random()*(14));
+	var secondPick = Math.floor(Math.random()*(14));
+	console.log(firstPick);
+	console.log(secondPick);
+
+	//check to see if previously picked numbers are selected 
+	if(picksThisSession.indexOf(firstPick) !== -1 || picksThisSession.indexOf(secondPick) !== -1){
+		console.log('at least one photo already shown');
+		randomTwoPicks();
+	}
+	//check if two identical numbers are selected
+	else if(firstPick === secondPick){
+		console.log('picks are the same number');
+		randomTwoPicks();
 	}
 	else{
+		console.log('selection success!');
 		selectPhotos(firstPick, secondPick);
 	}
-	
-
 }
 
 function selectPhotos(a,b){
-	//check for photos that have not been shown yet
-	while (catsThisSession > 1){
-	}
+	//ADD a way to check for photos that have not been shown yet below
+	//display photos from array based on the index of array and two random numbers
+	console.log(photoArray[a]);
+	console.log(photoArray[b]);
+	var $optionOne = photoArray[a].src;
+	var $optionTwo =  photoArray[b].src;
+	$('#img0').attr('src', $optionOne);
+	$('#img1').attr('src', $optionTwo);
+
 }
+
+ 
 
 initializePhotos();
 console.log(photoArray);
-randomTwoPics();
+randomTwoPicks();
 
